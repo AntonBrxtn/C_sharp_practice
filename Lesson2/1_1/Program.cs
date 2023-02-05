d9fdafc24d8278c1bdf948ee3ba1e94e7e4aa3eb
Anton Che <anton.chebykin@gmail.com>
2023-02-06 01:50:19 +1000

    Решена задача 1

---
+++ Lesson2/1_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит случайное трехзначное
+// число и удаляет вторую цифру этого числа
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+Random rand = new Random();
+int number = rand.Next(100, 1000);
+System.Console.WriteLine($"Случайное число: {number}");
+int leftNum = number / 100;
+int rightNum = number % 10;
+// System.Console.WriteLine($"{leftNum}{rightNum}");
+number = ((leftNum * 10) + (rightNum));
+System.Console.WriteLine(number);
