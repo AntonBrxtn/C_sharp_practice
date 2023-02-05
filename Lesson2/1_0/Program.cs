33883e77afea48d24844c4bb58bb8e065910a7ae
Anton Che <anton.chebykin@gmail.com>
2023-02-06 00:34:01 +1000

    1 задача

---
+++ Lesson2/1_0/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99]
+// и показывает наибольшую цифру числа
+
+Random rand = new Random();
+int number = rand.Next(10, 100);
+System.Console.WriteLine($"Случайное число: {number}");
+int leftNum = number / 10;
+int rightNum = number % 10;
+
+// System.Console.WriteLine($"{leftNum} {rightNum}");
+
+if (leftNum > rightNum)
+{
+    System.Console.WriteLine($"Левое число больше и равно {leftNum}");
+}
+else if (leftNum < rightNum)
+{
+    System.Console.WriteLine($"Правое число больше и равно {rightNum}");
+}
+else
+{
+    System.Console.WriteLine("Числа равны");
+}
