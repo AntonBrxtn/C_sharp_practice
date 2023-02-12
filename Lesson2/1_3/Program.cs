9122ebae881f24c6ce477cca653241d893a61be6
Anton Che <anton.chebykin@gmail.com>
2023-02-12 17:10:51 +1000

    Все задачи решены

---
+++ Lesson2/1_3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число
+// и проверяет, кратно ли оно одновременно 7 и 23.
+// 14 -> нет 
+// 46 -> нет 
+// 161 -> да
+
+System.Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 7&&23 == 0)
+{
+    System.Console.WriteLine("Число 2 кратно числу 1");
+}
+else 
+{
+    System.Console.WriteLine($"Не кратно. Остаток равен {number2 % number1}");
+}
