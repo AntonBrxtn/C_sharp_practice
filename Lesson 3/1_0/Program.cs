4d185f1bf593b0ea72a77a5cd63af7851caa7bcf
Anton Che <anton.chebykin@gmail.com>
2023-02-16 01:07:31 +1000

    seminar 3

--- Lesson 3/1_0/Program.cs
+++ Lesson 3/1_0/Program.cs
@@ -1,29 +1,69 @@
 ﻿// Задача №17. Напишите программу, которая принимает на вход координаты точки (X и Y), 
 // причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
 
-System.Console.WriteLine("Введите координату Х");
-int x = Convert.ToInt32(Console.ReadLine());
+// int InputNumber(string str)
+// {
+//     System.Console.WriteLine(str);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
 
-System.Console.WriteLine("Введите координату Y");
-int y = Convert.ToInt32(Console.ReadLine());
+// int x = InputNumber("Введите координату Х: ");
+// int y = InputNumber("Введите координату Y: ");
 
-if (x>0 && y>0)
+// if (x > 0 && y > 0)
+// {
+//     System.Console.WriteLine("Номер четверти 1");
+// }
+// else if (x < 0 && y < 0)
+// {
+//     System.Console.WriteLine("Номер четверти 3");
+// }
+// else if (x > 0 && y < 0)
+// {
+//     System.Console.WriteLine("Номер четверти 4");
+// }
+// else if (x < 0 && y > 0)
+// {
+//     System.Console.WriteLine("Номер четверти 2");
+// }
+// else
+// {
+//     System.Console.WriteLine("Точка лежит в начале координат");
+// }
+
+
+int InputNumber(string str)
 {
-    System.Console.WriteLine("Номер четверти 1");
+    System.Console.WriteLine(str);
+    return Convert.ToInt32(Console.ReadLine());
 }
-else if (x<0 && y<0)
+
+void FindQuarter(int a, int b)
 {
-    System.Console.WriteLine("Номер четверти 3");
+
+    if (a > 0 && b > 0)
+    {
+        System.Console.WriteLine("Номер четверти 1");
+    }
+    else if (a < 0 && b < 0)
+    {
+        System.Console.WriteLine("Номер четверти 3");
+    }
+    else if (a > 0 && b < 0)
+    {
+        System.Console.WriteLine("Номер четверти 4");
+    }
+    else if (a < 0 && b > 0)
+    {
+        System.Console.WriteLine("Номер четверти 2");
+    }
+    else
+    {
+        System.Console.WriteLine("Точка лежит в начале координат");
+    }
 }
-else if (x>0 && y<0)
-{
-    System.Console.WriteLine("Номер четверти 4");
-}
-else if (x<0 && y>0)
-{
-    System.Console.WriteLine("Номер четверти 2");
-}
-else
-{
-    System.Console.WriteLine("Точка лежит в начале координат");
-}
+
+int x = InputNumber("Введите координату Х: ");
+int y = InputNumber("Введите координату Y: ");
+
+FindQuarter (x, y);
---
+++ Lesson 3/1_2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход координаты
+// двух точек и находит расстояние между ними в 2D пространстве.
+
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+
+// ((x2-x1)^2 + (y2-y1)^2)^1/2
+// Math.Pow (число, количество квадратов)
+// Math.Sqrt (квадратный корень из указанного числа)
+
+int InputNumber (string str)
+{
+    System.Console.WriteLine(str);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int x1 = InputNumber("Введите координату х1: ");
+int y1 = InputNumber("Введите координату y1: ");
+
+int x2 = InputNumber("Введите координату х2: ");
+int y2 = InputNumber("Введите координату y2: ");
+
+System.Console.WriteLine(Math.Sqrt(Math.Pow((x2-x1),2) + Math.Pow ((y2-y1),2)));
