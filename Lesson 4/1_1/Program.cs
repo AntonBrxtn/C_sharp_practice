9bcfa38ddb4bfbbdb39f08546fe7b56f3d8c4296
Anton Che <anton.chebykin@gmail.com>
2023-02-21 01:31:27 +1000

    Семинар 4

---
+++ Lesson 4/1_1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход
+// число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+int GetNumber(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetFactorial(int N)
+{
+    int result = 1;
+    
+    for (int i = 1; i <= N; i++)
+    {
+        result *= i; // result = result + i;
+    }
+
+    return result;
+}
+
+int num = GetNumber("Введите число N: ");
+
+System.Console.WriteLine($"Произведение чисел от 1 до {num} = {GetFactorial(num)}");
---
+++ Lesson 4/1_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 26: Напишите программу, которая принимает на
+// вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+string GetNumber(string text)
+{
+    System.Console.WriteLine(text);
+    return Console.ReadLine();
+}
+
+string a = GetNumber("Введите число n");
+int len = a.Length;
+System.Console.WriteLine($"Количество цифр во введённом числе = {len}");
