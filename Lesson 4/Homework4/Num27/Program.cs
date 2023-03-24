81db5e809464fd383366047f8303c8ac3d2be51b
Anton Che <anton.chebykin@gmail.com>
2023-03-25 01:51:30 +1000

    Решена задача 27

---
+++ Lesson 4/Homework4/Num27/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int GetNumber(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Sum (int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        result += num % 10;
+        num = num / 10;
+    }
+    return result;
+}
+
+int num = GetNumber("Введите число: ");
+System.Console.WriteLine($"Сумма всех числе в цифре {num} = {Sum(num)}");
