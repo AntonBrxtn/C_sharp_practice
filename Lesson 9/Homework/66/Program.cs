994eef2a34e60aac1b9af47f1b819710d0645534
Anton Che <anton.chebykin@gmail.com>
2023-03-24 15:13:32 +1000

    Решена задача 66

---
+++ Lesson 9/Homework/66/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте значения M и N. Напишите программу,
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+point:
+System.Console.WriteLine("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n<=m)
+{
+    System.Console.WriteLine("Вероятно, вы ввели число, которого нет в промежутке. Пожалуйста, повторите ввод");
+    goto point;
+}
+
+int sum(int m, int n)
+{
+    if (m == n) { return m; }
+    return m + sum(m + 1, n);
+}
+System.Console.WriteLine($"Cумма натуральных элементов в промежутке от {m} до {n} = {sum(m, n)}");
+
